2bd61f7624dc67dff019fe13b6b417c2548626d0
wyattg71 <wyattgeorge1917@gmail.com>
2026-01-01 18:34:02 -0600

    add loras and models and nodes

--- wan_startup.sh
+++ wan_startup.sh
@@ -42,9 +42,20 @@ install_or_update () {
 }
 
 install_or_update https://github.com/ltdrdata/ComfyUI-Manager.git
-install_or_update https://github.com/Kosinkadink/ComfyUI-Advanced-ControlNet.git
-install_or_update https://github.com/Fannovel16/comfyui_controlnet_aux.git
-install_or_update https://github.com/WASasquatch/was-node-suite-comfyui.git
+install_or_update https://github.com/Kosinkadink/ComfyUI-VideoHelperSuite
+install_or_update https://github.com/city96/ComfyUI-GGUF
+install_or_update https://github.com/ltdrdata/ComfyUI-Impact-Pack
+install_or_update https://github.com/pythongosssss/ComfyUI-Custom-Scripts
+install_or_update https://github.com/chflame163/ComfyUI_LayerStyle
+install_or_update https://github.com/rgthree/rgthree-comfy
+install_or_update https://github.com/yolain/ComfyUI-Easy-Use
+install_or_update https://github.com/kijai/ComfyUI-KJNodes
+install_or_update https://github.com/1038lab/ComfyUI-RMBG
+install_or_update https://github.com/pollockjj/ComfyUI-MultiGPU
+install_or_update https://github.com/BadCafeCode/masquerade-nodes-comfyui
+install_or_update https://github.com/calcuis/gguf
+install_or_update https://github.com/KytraScript/ComfyUI_MatAnyone_Kytra
+install_or_update https://github.com/kael558/ComfyUI-GGUF-FantasyTalking
 
 # ---- Install Python requirements for custom nodes ----
 echo "Installing Python requirements..."
@@ -60,21 +71,25 @@ cd "$MODELS"
 
 echo "Downloading models..."
 
-# Checkpoints
-aria2c -c -x 8 -s 8 -d checkpoints \
-  https://huggingface.co/runwayml/stable-diffusion-v1-5/resolve/main/v1-5-pruned.ckpt
+hf download ashllay/YOLO_Models bbox/Eyeful_v2-Paired.pt --local-dir ultralytics/bbox
+hf download Kijai/WanVideo_comfy FastWan/Wan2_2-TI2V-5B-FastWanFullAttn_bf16.safetensors --local-dir diffusion_models
+hf download city96/umt5-xxl-encoder-gguf umt5-xxl-encoder-Q8_0.gguf --local-dir clip
+hf download QuantStack/Wan2.1_14B_VACE-GGUF Wan2.1_14B_VACE-Q8_0.gguf --local-dir unet
+hf download ussoewwin/Wan2.2_T2V_A14B_VACE-test_fp16_GGUF Wan2.2_T2V_High_Noise_14B_VACE_fp16.gguf --local-dir unet
+hf download ussoewwin/Wan2.2_T2V_A14B_VACE-test_fp16_GGUF Wan2.2_T2V_Low_Noise_14B_VACE_fp16.gguf --local-dir unet
 
-# VAE
 aria2c -c -x 8 -s 8 -d vae \
-  https://huggingface.co/stabilityai/sd-vae-ft-mse/resolve/main/sd-vae-ft-mse.safetensors
+  https://huggingface.co/Comfy-Org/Wan_2.1_ComfyUI_repackaged/resolve/main/split_files/vae/wan_2.1_vae.safetensors
 
 # LoRA (example)
-aria2c -c -x 8 -s 8 -d loras \
-  https://huggingface.co/latent-consistency/lcm-lora-sdv1-5/resolve/main/lcm-lora-sdv1-5.safetensors
-
-# ControlNet
-aria2c -c -x 8 -s 8 -d controlnet \
-  https://huggingface.co/lllyasviel/ControlNet-v1-1/resolve/main/control_v11p_sd15_canny.pth
+# https://civitai.com/models/1613190/wan-14b-boob-size-slider
+wget -P ComfyUI/models/loras "https://civitai.com/api/download/models/1825608?&token=dec1abd15d9725beeeaa7df9616c4eb1" --content-disposition
+# https://civitai.com/models/1307155/wan-22-experimental-wan-general-nsfw-model
+wget -P ComfyUI/models/loras "https://civitai.com/api/download/models/2073605?&token=dec1abd15d9725beeeaa7df9616c4eb1" --content-disposition
+wget -P ComfyUI/models/loras "https://civitai.com/api/download/models/2083303?&token=dec1abd15d9725beeeaa7df9616c4eb1" --content-disposition
+hf download DAKARA555/Wan_Breast_Helper_Hearmeman Wan_Breast_Helper_Hearmeman.safetensors --local-dir ComfyUI/models/loras
+# https://civitai.com/models/2109996/wan-22-pussy-and-anus-lora
+wget -P ComfyUI/models/loras "https://civitai.com/api/download/models/2387016?&token=dec1abd15d9725beeeaa7df9616c4eb1" --content-disposition
 
 # ---- Final message ----
 echo "=== Startup script finished ==="
